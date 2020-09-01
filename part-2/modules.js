1272adbc09b5392f9c38ee3afe491b9c83b80662
ijk <ijkten@no-email.com>
2020-08-31 22:47:03 -0400

    added solution for modules in part 2

---
+++ part-2/modules.js
@@ -0,0 +1,16 @@
+// Test 1: Create a function that returns a module
+// Return a Module that two fields first and second
+// pointing to str1 and str2 respectively.
+// In addition, module has a function 'myfunc' that
+// return concatenation of first and second.
+createModule = function(str1, str2) {
+  return (function() {
+      return {
+          first: str1,
+          second: str2,
+          myfunc: function() {
+              return this.first.concat(this.second);
+          }
+      }
+  })();
+}
